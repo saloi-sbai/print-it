94362b3433aeb2bcb525a19263100bd5808c9dae
saloi sbai <saloi.benoum@gmail.com>
2024-03-27 12:46:53 +0100

    fonctions updatedots et updatecarousel

--- assets/script.js
+++ assets/script.js
@@ -18,28 +18,54 @@ const slides = [
   },
 ];
 
-// pour ajouter les images dans la div banner
-const carousel = document.querySelector(".carousel");
+const bannerImg = document.querySelector(".banner-img");
+const arrowLeft = document.querySelector(".arrow_left");
+const arrowRight = document.querySelector(".arrow_right");
+const dots = document.querySelectorAll(".dot"); // Sélectionnez tous les points
 
-slides.forEach((item) => {
-  const carousel__item = document.createElement("div");
-  carousel__item.classList.add("carousel__item");
-  carousel__item.style.backgroundImage = `url(assets/images/slideshow/${item.image})`;
-  carousel__item.innerHTML = `<p class="tagline">${item.tagLine}</p>`;
-  carousel.appendChild(carousel__item);
-});
+let currentIndex = 0;
 
-let left = document.querySelector(".arrow_left");
-let right = document.querySelector(".arrow_right");
-let activeDot = document.querySelectorAll(".dots");
-
-// étape 3 les bullet point
-let dots = document.querySelector(".dots");
-for (let i = 0; i < slides.length; i++) {
-  let dot = document.createElement("span");
-  dot.classList.add("dot");
-  dots.appendChild(dot);
+// Fonction pour mettre à jour les points indicateurs
+function updateDots(index) {
+  dots.forEach((dot, i) => {
+    if (i === index) {
+      dot.classList.add("dot_selected"); // Ajoutez la classe pour le point actuel
+    } else {
+      dot.classList.remove("dot_selected"); // Supprimez la classe pour les autres points
+    }
+  });
 }
+
+// Fonction pour mettre à jour les points indicateurs, l'image et le texte
+function updateCarousel(index, direction) {
+      //correction du bug pour la première et la dernière image
+      if (currentIndex === -1 && direction === 'left') {
+        currentIndex = slides.length - 1;
+    } else if (currentIndex === slides.length && direction === 'right') {
+        currentIndex = 0;
+    }
+
+    // Mettre à jour l'image
+    const imagePath = `assets/images/slideshow/${slides[currentIndex].image}`;
+    bannerImg.src = imagePath;
+    bannerImg.alt = `Slide ${currentIndex + 1}`;
+
+    // Mettre à jour le texte
+    const tagLine = slides[currentIndex].tagLine;
+    document.querySelector('p').innerHTML = tagLine;
+
+    console.log(`Clic sur la flèche ${direction}`);
+}
+
+
+
+
+
+
+
+
+
+
 
 // au chargement de la page la premiere image et le premier point sont selectionnés
 const points = document.querySelectorAll(".dot");
